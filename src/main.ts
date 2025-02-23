2da30a56eda07f2951bf0f37a05d362336405cbf
Kevin Orge <kevinorge@gmail.com>
2025-02-23 16:00:01 +0800

    init basic crawling

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 // For more information, see https://crawlee.dev/
 import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
-
 import { router } from './routes.js';
 
-const startUrls = ['https://crawlee.dev'];
+async function crawlWebsite(url: string) {
 
-const crawler = new PlaywrightCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
-});
+    const crawler = new PlaywrightCrawler({
+        // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+        requestHandler: router,
+        // Comment this option to scrape the full website.
+        maxRequestsPerCrawl: 20,
+    });
 
-await crawler.run(startUrls);
+    let parsedUrl = null;
+    try {
+        parsedUrl = new URL(url);
+    } catch {
+        url = `https://${url}`;
+    }
+
+    await crawler.run([url]);
+}
+
+await crawlWebsite('seoprimehub.com');
--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,101 @@
-import { createPlaywrightRouter } from 'crawlee';
+import {createPlaywrightRouter, Dataset} from 'crawlee';
+import { fieldRules } from './rules.js';
 
 export const router = createPlaywrightRouter();
+const hostnames = new Set();
+const protocols = new Set();
+let dataSetId = "";
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
+const errors = new Map<string, any>();
+const addError = (field: string, message: string, elementTag: string | null = null) => {
+    if(!errors.has(field)) {
+        errors.set(field, []);
+    }
+    const fieldErrors = errors.get(field);
+    fieldErrors!.push({message: message, elementTag: elementTag});
+}
+
+// @ts-ignore
+const onPageSEO = async ({ request, page, log, parseWithCheerio }) => {
+    errors.clear();
+
+    const $ = await parseWithCheerio();
+
+    forLoop:
+        for (const fieldRule of fieldRules) {
+            let value = "";
+            let elementTag = "";
+            switch (fieldRule.name) {
+                case 'title':
+                    const title = await page.title();
+                    log.info(`${fieldRule.name}`, { url: request.loadedUrl });
+                    break;
+                case 'meta_description':
+                    const el = $('meta[name="description"]');
+                    value = el.prop('content');
+                    elementTag = el.toString();
+                    log.info(`${fieldRule.name}`, { description: value });
+                    break;
+                default:
+                    break forLoop;
+            }
+            for (const rule of fieldRule.rules) {
+                if(rule.func(value)) {
+                    addError(fieldRule.name, `${fieldRule.label} ${rule.message}`, elementTag);
+                }
+            }
+        }
+    for (const fieldRule of fieldRules) {
+        let elements = null;
+        switch (fieldRule.name) {
+            case 'image_alt':
+                elements = $('img');
+                break;
+        }
+        if(elements && elements.length > 0) {
+            elements.each((index:number, el: HTMLElement) => {
+                const value = $(el).prop('alt')
+                const elementTag = $(el).toString();
+                for (const rule of fieldRule.rules) {
+                    if(rule.func(value)) {
+                        addError(`${fieldRule.name}_${index}`, `${fieldRule.label} ${rule.message}`, elementTag);
+                    }
+                }
+            })
+        }
+    }
+
+    const dataset = await Dataset.open(dataSetId);
+    const _errors = {} as {[key: string]: string[]}
+    for(const [k, v] of errors) {
+        _errors[k] = v;
+    }
+    await dataset.pushData({ url: request.loadedUrl, errors: _errors});
+}
+
+router.addDefaultHandler(async ({ request, page, log, enqueueLinks, parseWithCheerio }) => {
+    const parsedUrl = new URL(request.loadedUrl);
+    hostnames.add(parsedUrl.hostname);
+    log.info(`Hostname: ${parsedUrl.hostname}`);
+    protocols.add(parsedUrl.protocol);
+    log.info(`Protocol: ${parsedUrl.protocol}`);
+    dataSetId = `${(new Date()).getTime()} - ${parsedUrl.hostname}`;
+    log.info(`Dataset id: ${dataSetId}`);
+
+    const dataset = await Dataset.open(dataSetId);
+    await dataset.pushData({
+        protocol: parsedUrl.protocol,
+        hostname: parsedUrl.hostname,
+        dataset: dataSetId,
+    });
+
+    await onPageSEO({ request, page, log, parseWithCheerio })
+
     log.info(`enqueueing new URLs`);
     await enqueueLinks({
-        globs: ['https://crawlee.dev/**'],
-        label: 'detail',
+        globs: [`${parsedUrl.protocol}//${parsedUrl.hostname}/**`],
+        label: 'on-page',
     });
 });
 
-router.addHandler('detail', async ({ request, page, log, pushData }) => {
-    const title = await page.title();
-    log.info(`${title}`, { url: request.loadedUrl });
-
-    await pushData({
-        url: request.loadedUrl,
-        title,
-    });
-});
+router.addHandler('on-page', onPageSEO);
