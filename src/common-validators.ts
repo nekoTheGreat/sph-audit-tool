b775b62976fbedde15a71bfa9360ed964db24391
Kevin Orge <kevinorge@gmail.com>
2025-02-25 12:40:36 +0800

    added more validation functions

--- src/common-validators.ts
+++ src/common-validators.ts
@@ -2,11 +2,34 @@ export function isEmpty(value: string) : boolean {
     return !value || value.length < 1;
 }
 
+export function isNotEmpty(value: string) : boolean {
+    return !isEmpty(value);
+}
+
 export function isStrLenGreater(value: string, limit: number) : boolean {
     if(!value) return false;
     return value.length > limit;
 }
 
+export function isStrLenGreaterOrEqual(value: string, limit: number) : boolean {
+    if(!value) return false;
+    return value.length >= limit;
+}
+
+export function isStrLenLess(value: string, limit: number) : boolean {
+    if(!value) return false;
+    return value.length < limit;
+}
+
+export function isStrLenLessOrEqual(value: string, limit: number) : boolean {
+    if(!value) return false;
+    return value.length <= limit;
+}
+
 export function isStrDuplicate(value: string, uniqueItems: Set<string>) : boolean {
     return uniqueItems.has(value);
+}
+
+export function isStrUnique(value: string, uniqueItems: Set<string>) : boolean {
+    return !uniqueItems.has(value);
 }
