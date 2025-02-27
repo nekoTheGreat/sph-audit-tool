1c2406125c3390546c1a5c0ce8cc9a77ab0deb4f
Kevin Orge <kevinorge@gmail.com>
2025-02-27 13:02:54 +0800

    new validate return type

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,16 @@ import {CheerioRoot} from "crawlee"
 import Cheerio = cheerio.Cheerio;
 
 export interface SeoFieldRule {
-    validate({ value, el } : { value: string, el: cheerio.Cheerio}): boolean,
+    name: string,
+    validate({ value, el } : { value: string, el: cheerio.Cheerio}): SeoFieldRuleResult,
     errorMessage: string,
 }
+
+export interface SeoFieldRuleResult {
+    valid: boolean,
+    skipRules?: string[] | undefined,
+}
+
 export interface SEOField {
     name: string,
     label: string,
