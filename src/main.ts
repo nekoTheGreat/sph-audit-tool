876530b86c8cc5fe6d91646cd2788d89bd632bf6
Kevin Orge <kevinorge@gmail.com>
2025-03-16 15:35:56 +0800

    basic auth of the root url

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req: Request, res: Response, next) => {
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+    if(login == process.env.BASIC_AUTH_USER && password == process.env.BASIC_AUTH_PASS) {
+        return next();
+    }
+    res.set('WWW-Authenticate', 'Basic realm="401"');
+    res.status(401).send('Authentication required.');
+});
+
 app.get('/', (_: Request, res: Response) => {
     res.send('Good day...');
 });
