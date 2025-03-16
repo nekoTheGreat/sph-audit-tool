9715903bd8ab5f22d835496cb8b766e42c026964
Kevin Orge <kevinorge@gmail.com>
2025-03-16 13:43:24 +0800

    process keywords of a page: word's density >= .5 or <= 2.5

--- src/parsers/page-parser.ts
+++ src/parsers/page-parser.ts
@@ -22,6 +22,11 @@ export class PageParser {
      */
     public group: string = '';
 
+    /**
+     * Store keywords of a page
+     */
+    keywords: {word: string, count: number, density: number}[] = [];
+
     constructor(url: string, $: CheerioRoot) {
         if(!url.startsWith("http")) {
             url = "https://" + url;
@@ -79,11 +84,47 @@ export class PageParser {
         return audits;
     }
 
+    processKeywords({ $ } : { $: CheerioRoot }) {
+        const htmlText = $('body').text().toLowerCase();
+        const keywordsCounter = new Map<string, number>();
+        const ignoreWords = ['a', 'the', 'and'];
+        const words = htmlText.split(' ');
+        for(let word of words) {
+            word = word.trim().replaceAll(/^a-zA-Z0-9/gi, '') ?? '';
+            if(word.length == 0 || ignoreWords.includes(word)) continue;
+            let counter = 1;
+            if(keywordsCounter.has(word)) {
+                counter = keywordsCounter.get(word)! + 1;
+            }
+            keywordsCounter.set(word, counter)
+        }
+
+        const totalWords = keywordsCounter.size;
+        if(totalWords > 0) {
+            for(const [k, v] of keywordsCounter) {
+                const density = parseFloat((v / totalWords * 100).toFixed(3)) ;
+                if(density < .5 || density > 2.5) continue;
+                this.keywords.push({word: k, count: v, density: density});
+            }
+        }
+        this.keywords.sort(function(a, b) {
+            if(a.density > b.density) return 1;
+            else if(a.density < b.density) return -1;
+            return 0;
+        });
+    }
+
+    getKeywords() {
+        return this.keywords;
+    }
+
     /**
      * Iterate seoFields property, get each value and check if it's valid and returns the audits
      * @param $
      */
     async parse({ $ } : { $: CheerioRoot }) : Promise<ParserResult>  {
+        this.processKeywords({ $ });
+
         let resultPromises: Promise<SeoFieldRuleResult>[] = [];
         for(const seoField of this.seoFields) {
             resultPromises.push(seoField.validate({ $, url: this.url }));
