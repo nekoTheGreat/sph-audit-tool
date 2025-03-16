eba73fbe0831d501bdbac7ed0b64b6999e5bf1b2
Kevin Orge <kevinorge@gmail.com>
2025-03-16 15:26:45 +0800

    simple route to crawl a url

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import {ParserResult} from "./types.js";
+import {crawl} from "./basic-crawl.js";
 
 dotenv.config();
 
@@ -9,6 +11,22 @@ const port = process.env.PORT || 3000;
 app.get('/', (_: Request, res: Response) => {
     res.send('Good day...');
 });
+
+app.get('/crawl', async (req: Request, res: Response) => {
+    let resp: { error: string } | ParserResult[];
+    let statusCode = 200;
+    const url = req.query.url?.toString() ?? '';
+    try {
+        if(!url) throw "URL required";
+        resp = await crawl(url);
+    } catch(e: any) {
+        statusCode = 400;
+        resp = { error: e.toString() };
+    }
+
+    res.statusCode = statusCode;
+    res.send(resp);
+})
 
 app.listen(port, () => {
     console.log(`Server is running...`);
