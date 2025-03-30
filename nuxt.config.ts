83a2dde4952a0b98ff41162b0c91eaf43eca4fd7
Kevin Orge <kevinorge@gmail.com>
2025-03-30 16:35:41 +0800

    installed auth utils and setup nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', 'nuxt-auth-utils'],
   css: ['./assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+  runtimeConfig: {
+    auth: {
+      user: process.env.BASIC_AUTH_USER,
+      pass: process.env.BASIC_AUTH_PASS,
+    }
+  }
 })
