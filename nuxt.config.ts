474fcc4720fab99ef90e821ce360e4f4f1cab2bc
Kevin Orge <kevinorge@gmail.com>
2025-03-30 10:03:57 +0800

    configure nuxt/ui, tailwindcss and main css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
   css: ['./assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
