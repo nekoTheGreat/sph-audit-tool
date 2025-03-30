99f80b444a0f20638f2529b1342325f20ad92934
Kevin Orge <kevinorge@gmail.com>
2025-03-30 10:34:40 +0800

    auth form components

---
+++ components/AuthForm/AuthFormPanel.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="max-w-xs mx-auto border rounded-sm p-5 shadow">
+    <h1 class="font-bold text-center">SPH Audit Tool</h1>
+    <AuthForm></AuthForm>
+  </div>
+</template>
