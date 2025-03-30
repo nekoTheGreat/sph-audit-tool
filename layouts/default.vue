864baea0d87a1567d510931468ab890535c31e18
Kevin Orge <kevinorge@gmail.com>
2025-03-30 10:04:13 +0800

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col h-full">
+    <main style="flex: 1">
+      <slot/>
+    </main>
+    <div class="text-center text-xs" style="height: 10px;">
+      This is only a test page.
+    </div>
+  </div>
+</template>
