5685fb2fc765c22aa640d2b42e9820057a327612
Kevin Orge <kevinorge@gmail.com>
2025-03-30 18:05:08 +0800

    added top nav component to home

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-
+import TopNav from "~/components/Nav/TopNav.vue";
 const { user } = useUserSession();
 </script>
 
 <template>
-  <div class="@container/main mt-5">
+  <TopNav></TopNav>
+  <div class="@container/main mt-5 p-5">
     Welcome, {{ user.name }}
   </div>
 </template>
