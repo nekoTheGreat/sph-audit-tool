4a63af9bfdf1954eb995bf5d2994ff57c8264f82
Kevin Orge <kevinorge@gmail.com>
2025-03-31 12:43:55 +0800

    simple form for audit tool

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
 import TopNav from "~/components/Nav/TopNav.vue";
-const { user } = useUserSession();
+import {z} from "zod";
+import type {FormSubmitEvent} from "@nuxt/ui";
+
+const state = reactive({
+  url: '',
+})
+const schema = z.object({
+  url: z.string().url()
+})
+async function onSubmit(event: FormSubmitEvent<any>) {
+  try {
+
+  } catch (e) {
+
+  }
+}
 </script>
 
 <template>
   <TopNav></TopNav>
   <div class="@container/main mt-5 p-5">
-    Welcome, {{ user.name }}
+    <div class="max-w-md mx-auto p-5 shadow">
+      <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+        <UFormField label="URL" name="url">
+          <UInput class="w-full" v-model="state.url" type="text" />
+        </UFormField>
+        <UButton type="submit">
+          Submit
+        </UButton>
+      </UForm>
+    </div>
   </div>
 </template>
